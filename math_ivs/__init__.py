91b393a829553b127e7ca34290a20456e1dc5ff6
Samleyz <samuel.cehlarik@gmail.com>
2026-04-28 22:26:47 +0200

    fixed sqrt, cant place two operators next to each other , factorial fixed

--- math_ivs/__init__.py
+++ math_ivs/__init__.py
@@ -2,6 +2,8 @@ def sqrt(x):
     return x**(1/2)
 def factorial(x):
     res = 1
+    if x < 0:
+        raise ValueError("Negative factorial not allowed")
     for i in range(x+1):
         if i == 0:
             continue
